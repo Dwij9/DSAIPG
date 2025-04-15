f79cbc0b5c7b176b62613b7524f5eae89d04d9f0
Dwij <42746391+Dwij9@users.noreply.github.com>
2025-04-14 21:51:56 -0400

    Update ThreeSumQuadraticWithCalipers.java

--- src/main/java/com/phasmidsoftware/dsaipg/adt/threesum/ThreeSumQuadraticWithCalipers.java
+++ src/main/java/com/phasmidsoftware/dsaipg/adt/threesum/ThreeSumQuadraticWithCalipers.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2024. Robin Hillyard
- */
-
 package com.phasmidsoftware.dsaipg.adt.threesum;
 
 import java.util.ArrayList;
@@ -10,51 +6,67 @@ import java.util.List;
 import java.util.function.Function;
 
 /**
- * Implementation of ThreeSum which follows the approach of dividing the solution-space into
- * N sub-spaces where each subspace corresponds to a fixed value for the middle index of the three values.
- * Each subspace is then solved by expanding the scope of the other two indices outwards from the starting point.
- * Since each subspace can be solved in O(N) time, the overall complexity is O(N^2).
- * <p>
- * The array provided in the constructor MUST be ordered.
+ * Implementation of ThreeSum using a "calipers" approach.
+ * This class requires the input array to be sorted.
  */
 public class ThreeSumQuadraticWithCalipers implements ThreeSum {
+
     /**
-     * Construct ints ThreeSumQuadratic on ints.
+     * Constructor requires a sorted array.
      *
-     * @param ints a sorted array.
+     * @param ints a sorted array of integers.
      */
     public ThreeSumQuadraticWithCalipers(int[] ints) {
         this.a = ints;
-        length = ints.length;
+        this.length = ints.length;
     }
 
     /**
-     * Get an array or Triple containing all of those triples for which sum is zero.
+     * Get all unique Triples where the sum is zero.
      *
-     * @return a Triple[].
+     * @return an array of Triple objects.
      */
     public Triple[] getTriples() {
         List<Triple> triples = new ArrayList<>();
-        Collections.sort(triples); // ???
-        for (int i = 0; i < length - 2; i++)
+        for (int i = 0; i < length - 2; i++) {
             triples.addAll(calipers(a, i, Triple::sum));
+        }
         return triples.stream().distinct().toArray(Triple[]::new);
     }
 
     /**
-     * Get a set of candidate Triples such that the first index is the given value i.
-     * Any candidate triple is added to the result if it yields zero when passed into function.
+     * Performs a two-pointer search from both ends, fixing a[i] as the first element.
      *
-     * @param a        a sorted array of ints. This method is concerned only with the partition of a starting with index i+1.
-     * @param i        the index of the first element of resulting triples.
-     * @param function a function which takes a triple and returns a value which will be compared with zero.
-     * @return a List of Triples.
+     * @param a        a sorted array of ints.
+     * @param i        the index of the first element in the triple.
+     * @param function function to test the triple (e.g., checking if sum == 0).
+     * @return a list of valid triples.
      */
     public static List<Triple> calipers(int[] a, int i, Function<Triple, Integer> function) {
         List<Triple> triples = new ArrayList<>();
-        // TO BE IMPLEMENTED  : use function to qualify triples and to navigate otherwise.
-         return null;
-        // END SOLUTION
+        int left = i + 1;
+        int right = a.length - 1;
+
+        while (left < right) {
+            Triple t = new Triple(a[i], a[left], a[right]);
+            int value = function.apply(t);
+
+            if (value == 0) {
+                triples.add(t);
+                left++;
+                right--;
+
+                // Skip duplicates
+                while (left < right && a[left] == a[left - 1]) left++;
+                while (left < right && a[right] == a[right + 1]) right--;
+
+            } else if (value < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return triples;
     }
 
     private final int[] a;
